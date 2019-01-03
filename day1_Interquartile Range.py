b66220ce89fb42db4437f5b474d6b0088812d081
Daria <dmetafrasis@gmail.com>
2019-01-04 04:56:45 +0700

    added file, day 1, part 3

---
+++ day1_Interquartile Range.py
@@ -0,0 +1,22 @@
+import statistics as stats
+
+n = int(input())
+X = list(map(int, input().split()))
+F = list(map(int, input().split()))
+
+# calculating interquartile_range
+S = []
+for i in range(n):
+    S += [X[i]] * F[i]
+N = sum(F)
+S.sort()
+
+if n%2 != 0:
+    quartile1 = stats.median(S[:N//2])
+    quartile3 = stats.median(S[N//2 + 1:])
+else:
+    quartile1 = stats.median(S[:N//2])
+    quartile3 = stats.median(S[N//2:])
+
+interquartile_range = round(float(quartile3 - quartile1), 1)
+print(interquartile_range)
